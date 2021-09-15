9d20b67f6a9ce0806a23f1ebdbac94db1dd0230b
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-09-15 12:36:26 +0900

    c031

--- c031.js
+++ c031.js
@@ -1,7 +1,6 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-// 自分の得意な言語で
-// Let's チャレンジ！！
+
 var lines = [];
 var reader = require('readline').createInterface({
   input: process.stdin,
@@ -11,55 +10,70 @@ reader.on('line', (line) => {
   lines.push(line);
 });
 reader.on('close', () => {
+  const ALL_DATA = lines
+
   // 都市数
-  const CITY_COUNT = lines[0]
+  const CITY_COUNT = ALL_DATA[0];
+
   // 投稿場所,日時
-  const POST_DATA = lines.slice(-1)[0]
-  const GET_TIMESTAMP = POST_DATA.split(' ')
-  const GET_TIME = GET_TIMESTAMP[1].split(':')
-  const GET_HOUR = Number(GET_TIME[0])
-  const GET_MINITE = GET_TIME[1]
-  // 基準となる時差を取得
-  const GET_STANDARD = POST_DATA.split(/\s/)
-  const GET_CITY = GET_STANDARD[0]
-  // 都市を取得
-  const CITYS = []
-  // 時差を取得
-  const TIME_LAG = []
-  for (let i = 1; i < lines.length - 1; i += 1) {
-    let tmp = []
-    tmp = lines[i].split(/\s/)
-    CITYS.push(tmp[0])
-    TIME_LAG.push(Number(tmp[1]))
+  const POST_DATA = ALL_DATA.slice(-1)[0];
+  const TIMESTAMP = POST_DATA.split(/\s/);
+  const TIMESTAMP_ARY = TIMESTAMP[1].split(':');
+
+  // 基準となる時間
+  const STANDARD_HOUR = Number(TIMESTAMP_ARY[0]);
+  const STANDARD_MINITE = TIMESTAMP_ARY[1];
+
+  // 基準となる都市を取得
+  const TIMELAG_ARY = POST_DATA.split(/\s/);
+  const STANDARD_CITY = TIMELAG_ARY[0];
+
+  // 各都市を取得 各時差を取得
+  const CITYS_ARY = [];
+  const TIMELAGS_ARY = [];
+  for (let i = 1; i < ALL_DATA.length - 1; i += 1) {
+    let tmp = [];
+    tmp = ALL_DATA[i].split(/\s/);
+    CITYS_ARY.push(tmp[0]);
+    TIMELAGS_ARY.push(Number(tmp[1]));
   }
-  const CITY_POSITION = CITYS.indexOf(GET_CITY) + 1
-  const GET_STANDARD_CITY = lines[CITY_POSITION].split(/\s/)
-  const STANDARD_TIME = Number(GET_STANDARD_CITY[1])
-  // 時差を調整 // わからない...
-  const CONVERT_TIME_LAG = []
-  let tmp = 0
-  for (let i = 0; i < TIME_LAG.length; i+= 1) {
-    if (TIME_LAG[i] === STANDARD_TIME) {
-      tmp = GET_HOUR
-    } else if (Math.sign(TIME_LAG[i]) === 0) {
-      tmp = GET_HOUR - STANDARD_TIME
-    } else if (Math.sign(TIME_LAG[i]) === 1) {
-      tmp = GET_HOUR + TIME_LAG[i]
-      if (tmp > 24) { tmp -= STANDARD_TIME }
-    } else if  (Math.sign(TIME_LAG[i]) === -1) {
-      tmp = GET_HOUR - Math.abs(TIME_LAG[i])
-      tmp = tmp - STANDARD_TIME
-      if (tmp > 24) { tmp -= STANDARD_TIME }
+
+  // 配列内に基準となる都市が含まれていないか
+  const STANDARD_CITY_POSITION = CITYS_ARY.indexOf(STANDARD_CITY) + 1;
+  const STANDARD_CITY_ARY = ALL_DATA[STANDARD_CITY_POSITION].split(/\s/);
+  const STANDARD_CITY_TIME = Number(STANDARD_CITY_ARY[1]);
+
+  // 各都市の時差を調整する
+  const CONVERT_TIME_LAG = [];
+  let tmp = 0;
+  for (let i = 0; i < TIMELAGS_ARY.length; i+= 1) {
+    // 基準となる都市と時間が一致
+    if (CITYS_ARY[i] === STANDARD_CITY) {
+      tmp = Math.abs(STANDARD_HOUR + TIMELAGS_ARY[i] - STANDARD_CITY_TIME);
+    } else if (Math.sign(TIMELAGS_ARY[i]) === 0) {
+      tmp = Math.abs(STANDARD_HOUR + TIMELAGS_ARY[i] - STANDARD_CITY_TIME);
+    } else if (Math.sign(TIMELAGS_ARY[i]) === 1) {
+      tmp = STANDARD_HOUR + TIMELAGS_ARY[i] - STANDARD_CITY_TIME;
+      if (Math.sign(tmp) === -1) { tmp+= 24; }
+      tmp = Math.abs(tmp)
+    } else if (Math.sign(TIMELAGS_ARY[i]) === -1) {
+      tmp = STANDARD_HOUR + TIMELAGS_ARY[i] - STANDARD_CITY_TIME;
+      if (Math.sign(tmp) === -1) { tmp+= 24; }
+      tmp = Math.abs(tmp)
     }
-    tmp = String(tmp)
+    // 桁調整
+    tmp = String(tmp);
     if (tmp.length < 2) { tmp = `0${tmp}` }
-    CONVERT_TIME_LAG.push(tmp)
+    CONVERT_TIME_LAG.push(tmp);
   }
-//   console.log(CONVERT_TIME_LAG)
+
+  // 出力用データの作成
   const POST_DATAS = []
   for (let i = 0; i < CONVERT_TIME_LAG.length; i+= 1) {
-    let tmp = `${CONVERT_TIME_LAG[i]}:${GET_MINITE}` 
+    let tmp = `${CONVERT_TIME_LAG[i]}:${STANDARD_MINITE}`
     POST_DATAS.push(tmp)
   }
+
+  // 出力
   console.log(POST_DATAS.join('\n'))
 });
