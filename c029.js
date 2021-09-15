81a212edd9357cb051e741e83d1d952afd5708cd
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-09-15 17:31:29 +0900

    c029

--- c029.js
+++ c029.js
@@ -1,3 +1,4 @@
+// This is a code with a 100% pass rate.
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
@@ -10,12 +11,14 @@ reader.on('line', (line) => {
   lines.push(line);
 });
 reader.on('close', () => {
-  // 連休,日程
+  // input
   const TRAVELL_DAYS = lines[0].split(/\s/).map(Number)
-  // 連休期間数,日程期間数
+
+  // total holidays
   const HOLDAYS = TRAVELL_DAYS[0]
+  // total periods
   const PERIODS = TRAVELL_DAYS[1]
-  // 連休期間中の天気
+  // all holidays /  all weathers
   const HOLDAYS_ARY = []
   const WEATHERS_ARY = []
   for (let i = 1;i < lines.length; i += 1) {
@@ -23,24 +26,27 @@ reader.on('close', () => {
     HOLDAYS_ARY.push(travellDays[0])
     WEATHERS_ARY.push(travellDays[1])
   }
-  // 日程期間中の配列
+
+  // comparison of periods
   const COMPARISON_WEATHERS = []
   const COMPARISON_WEATHERS_RANGE = HOLDAYS - PERIODS + 1
-  // 連休期間中の天気
   for (let i = 0; i < COMPARISON_WEATHERS_RANGE; i += 1) {
-    // iから日程期間分の合計値を出す
+    // probability of precipitation during the period
     let sumWeathers = 0
     for (let j = 0; j < PERIODS; j += 1) {
       sumWeathers += WEATHERS_ARY[j+i]
     }
     COMPARISON_WEATHERS.push(sumWeathers)
   }
-  // 降水確率の最小値を求める
+
+  // best weather day
   const MIN_WEATHER = COMPARISON_WEATHERS.reduce((a,b) => {
     return Math.min(a,b)
   })
   const MIN_WEATHER_INDEX = COMPARISON_WEATHERS.indexOf(MIN_WEATHER)
   const STRAT_DATE = HOLDAYS_ARY[0] + MIN_WEATHER_INDEX
   const END_DATE = HOLDAYS_ARY[0] + MIN_WEATHER_INDEX + PERIODS - 1
+
+  // output
   console.log(`${STRAT_DATE} ${END_DATE}`)
 });
