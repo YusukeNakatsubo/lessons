6b4a9c8646424b003a81912fb25b452d6337b581
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-09-28 14:20:55 +0900

    c046

--- c005.js
+++ c005.js
@@ -11,18 +11,18 @@ reader.on('line', (line) => {
 });
 reader.on('close', () => {
   const INPUT = lines;
-
+  //
   const inputIp = [];
   const getRange = (from, to) => {
     let newArray = [];
     for(let i = from; i <= to; i += 1) { newArray.push(i); }
     return newArray;
   }
-
+  //
   for (let i = 1; i < INPUT.length; i += 1) {
     inputIp.push(INPUT[i]);
   }
-
+  //
   for (let i = 0; i < inputIp.length; i += 1) {
     let iTmp = inputIp[i].split('.');
     if(iTmp.length !== 4) {
---
+++ c046.js
@@ -0,0 +1,50 @@
+// This is a code with a 100% pass rate.
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUT        = lines;
+        MEMBER_COUNT = Number(INPUT[0]),
+        MEMBERS      = INPUT[1].split(/\s/),
+        BOOK_COUNT   = Number(INPUT[0]);
+  //
+  let bookList = [];
+  for (let i = 3; i < INPUT.length; i += 1) {
+    let tmp = INPUT[i].split(/\s/);
+    let obj = {};
+    obj['personName'] = tmp[0];
+    obj['bookPrice'] = Number(tmp[1]);
+    bookList.push(obj);
+  }
+  //
+  let memberList = [];
+  for (let i = 0; i < MEMBER_COUNT; i += 1) {
+    let obj = {};
+    obj['personName'] = MEMBERS[i];
+    obj['bookPrice'] = 0;
+    memberList.push(obj);
+  }
+  //
+  for (let i = 0; i < memberList.length; i += 1) {
+    for (let j = 0; j < bookList.length; j += 1) {
+      if (memberList[i]['personName'] === bookList[j]['personName']) {
+        memberList[i]['bookPrice'] += bookList[j]['bookPrice'];
+      }
+    }
+  }
+  //
+  let bookListRank = []
+  let sort_target = 'bookPrice';
+  bookListRank = memberList.sort((a, b) => b[sort_target] - a[sort_target]);
+  //
+  for (let rank in bookListRank) {
+    console.log(bookListRank[rank]['personName']);
+  }
+});
