bfde3c8ce8ff7ad72118e06b0a92de00b294f152
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-09-29 17:49:20 +0900

    c023

---
+++ c023.js
@@ -0,0 +1,25 @@
+// // This is a code with a 100% pass rate.
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const HITS  = lines[0].split(/\s/).map(Number),
+        COUNT = Number(lines[1]);
+  for (let i = 0; i < COUNT; i += 1) {
+    let tmpLotte = lines[i+2].split(/\s/).map(Number);
+    let tmpHits = [];
+    for (let j = 0; j < tmpLotte.length; j += 1) {
+      if (tmpLotte.includes(HITS[j]) === true) {
+        tmpHits.push(HITS[j]);
+      }
+    }
+    console.log(tmpHits.length);
+  }
+});
