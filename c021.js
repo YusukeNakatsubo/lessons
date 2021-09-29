13962640388669a8e77853d9b9f7d8f39c6f6d5a
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-09-29 13:51:53 +0900

    c021

--- c021.js
+++ c021.js
@@ -10,17 +10,17 @@ reader.on('line', (line) => {
   lines.push(line);
 });
 reader.on('close', () => {
-  const INPUT = lines[0].split(/\s/).map(Number),
-        x     = INPUT[0],
-        y     = INPUT[1],
-        r_1   = INPUT[2],
-        r_2   = INPUT[3],
-        COUNT = Number(lines[1]);
+  const INPUT      = lines[0].split(/\s/).map(Number),
+        POSITION_X = INPUT[0],
+        POSITION_Y = INPUT[1],
+        RADIUS01   = INPUT[2],
+        RADIUS02   = INPUT[3],
+        COUNT      = Number(lines[1]);
   //
   for (let i = 0; i < COUNT; i += 1) {
     let tmp = lines[i+2].split(/\s/).map(Number);
-    let tmpPosition = Math.pow((x - tmp[0]),2) + Math.pow((y - tmp[1]),2);
-    if (Math.pow(r_1,2) <= tmpPosition && tmpPosition <= Math.pow(r_2,2)) {
+    let tmpPosition = Math.pow((POSITION_X - tmp[0]),2) + Math.pow((POSITION_Y - tmp[1]),2);
+    if (Math.pow(RADIUS01,2) <= tmpPosition && tmpPosition <= Math.pow(RADIUS02,2)) {
       console.log('yes');
     } else {
       console.log('no');
