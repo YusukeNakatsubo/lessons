9bd6c10d0db563536c135185491e5c07a4165fc3
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-01 17:49:00 +0900

    c025.js

---
+++ c025.js
@@ -0,0 +1,35 @@
+// This is a code with a 100% pass rate. why?
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        POWER  = Number(INPUTS[0]),
+        COUNT  = Number(INPUTS[1]);
+  //
+  let hourCounter = 0,
+      amountCounter = 0,
+      actionCounter = 0;
+  for (let i = 0; i < COUNT; i += 1) {
+    let tmpHour, tmpTime, tmpAmount;
+    [tmpHour, tmpTime, tmpAmount] = INPUTS[i+2].split(/\s/).map(Number);
+    if (tmpHour !== hourCounter) {
+      // 行動回数のカウンターを進める
+      actionCounter = actionCounter + Math.ceil(amountCounter / POWER);
+      // 時間のカウンターを進める
+      hourCounter = tmpHour;
+      // 量のカウンターを進める
+      amountCounter = tmpAmount;
+    } else {
+      amountCounter += tmpAmount;
+    }
+  }
+  console.log(actionCounter + Math.ceil(amountCounter / POWER));
+});
