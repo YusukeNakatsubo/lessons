5aafd0804f24f36682db6732449174e773c586f1
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-05 11:07:30 +0900

    c032

--- c034.js
+++ c034.js
@@ -10,26 +10,12 @@ reader.on('line', (line) => {
   lines.push(line);
 });
 reader.on('close', () => {
-  const INPUT        = lines[0].split(/\s/),
-        ADDITION     = INPUT.indexOf('+'),
-        SUBTRACTION  = INPUT.indexOf('-');
+  const INPUT = lines[0].split(/\s/);
   if (INPUT[0] === 'x') {
-    if (ADDITION !== -1) {
-      console.log(Number(INPUT[4]) - Number(INPUT[2]));
-    } else if (SUBTRACTION[0] !== -1) {
-      console.log(Number(INPUT[4]) + Number(INPUT[2]));
-    }
+    INPUT[1] === '+' ? console.log(Number(INPUT[4]) - Number(INPUT[2])) : console.log(Number(INPUT[4]) + Number(INPUT[2]));
   } else if (INPUT[2] === 'x') {
-    if (ADDITION !== -1) {
-      console.log(Number(INPUT[4]) - Number(INPUT[1]));
-    } else if (SUBTRACTION[0] !== -1) {
-      console.log(Number(INPUT[0]) - Number(INPUT[4]));
-    }
+    INPUT[1] === '+' ? console.log(Number(INPUT[4]) - Number(INPUT[1])) : console.log(Number(INPUT[0]) - Number(INPUT[4]));
   } else if (INPUT[4] === 'x') {
-    if (ADDITION !== -1) {
-      console.log(Number(INPUT[0]) + Number(INPUT[2]));
-    } else if (SUBTRACTION[0] !== -1) {
-      console.log(Number(INPUT[0]) - Number(INPUT[2]));
-    }
+    INPUT[1] === '+' ? console.log(Number(INPUT[0]) + Number(INPUT[2])) : console.log(Number(INPUT[0]) - Number(INPUT[2]));
   }
 });
