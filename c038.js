f06f6773f81ba0aacfae04c55d512a898ef8a1b7
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-05 17:45:14 +0900

    c038

---
+++ c038.js
@@ -0,0 +1,32 @@
+// This is a code with a 90% pass rate. Why?
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS        = lines,
+        INPUT         = INPUTS[0].split(/\s/).map(Number),
+        MACHINE_COUNT = INPUT[0],
+        SWEETS_COUNT  = INPUT[1];
+  let sweetsCount  = SWEETS_COUNT,
+      machineCount = 0,
+      machineIndex = 0;
+  for (let i = 0; i < MACHINE_COUNT; i += 1) {
+    let tmpMachineCount = Number(INPUTS[i+1]);
+    if (SWEETS_COUNT % tmpMachineCount < sweetsCount) {
+      sweetsCount = sweetsCount % tmpMachineCount;
+      machineCount = tmpMachineCount;
+      machineIndex = i + 1;
+    } else if (SWEETS_COUNT % tmpMachineCount === sweetsCount && machineCount < tmpMachineCount) {
+      machineCount = tmpMachineCount;
+      machineIndex = i + 1;
+    }
+  }
+  console.log(machineIndex);
+});
