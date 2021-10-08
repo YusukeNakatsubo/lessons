9c03d072ae95482f3f579c0fde5418b0e0c942f9
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-08 12:23:32 +0900

    c044

--- c044.js
+++ c044.js
@@ -1,4 +1,4 @@
-// This is a code with a 50% pass rate. Why?
+// This is a code with a 80% pass rate. Why?
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 var lines = [];
@@ -16,22 +16,18 @@ reader.on('close', () => {
   for (let i = 0; i < NUMBER; i += 1) {
     hands.push(INPUTS[i+1]);
   }
-  const rocks = hands.filter((h) => { return h === 'rock'; })
-  const scissores = hands.filter((h) => { return h === 'scissors'; })
-  const papers = hands.filter((h) => { return h === 'paper'; })
-  if (rocks.length && scissores.length && papers.length) {
+  const ROCK_COUNT     = (hands.filter((h) => { return h === 'rock'; })).length,
+        SCISSORS_COUNT = (hands.filter((h) => { return h === 'scissors'; })).length,
+        PAPER_COUNT    = (hands.filter((h) => { return h === 'paper'; })).length;
+  if (ROCK_COUNT > 0 && SCISSORS_COUNT > 0 && PAPER_COUNT > 0) {
     console.log('draw');
-  } else if (rocks.length && !scissores.length && !papers.length) {
+  } else if (ROCK_COUNT === SCISSORS_COUNT || SCISSORS_COUNT === PAPER_COUNT || PAPER_COUNT === ROCK_COUNT) {
     console.log('draw');
-  } else if (!rocks.length && scissores.length && !papers.length) {
-    console.log('draw');
-  } else if (!rocks.length && !scissores.length && papers.length) {
-    console.log('draw');
-  } else if (rocks.length > scissores.length && !papers.length) {
-    console.log('rock');
-  } else if (scissores.length > papers.length && !rocks.length) {
+  } else if (hands.includes('rock') === false) {
     console.log('scissors');
-  } else if (papers.length > rocks.length && !scissores.length) {
+  } else if (hands.includes('scissors') === false) {
     console.log('paper');
+  } else if (hands.includes('paper') === false) {
+    console.log('rock');
   }
 });
