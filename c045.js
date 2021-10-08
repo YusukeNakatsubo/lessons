d71934a6e1d4983128489581fc945a7a97894649
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-08 15:04:59 +0900

    c045

--- c045.js
+++ c045.js
@@ -10,12 +10,10 @@ reader.on('line', (line) => {
   lines.push(line);
 });
 reader.on('close', () => {
-  const INPUTS       = lines[0].split(/\s/).map(Number),
-        PAGE_VOLUME  = INPUTS[0],
-        PAGE_SIZE    = INPUTS[1],
-        PAGE_SHOW    = INPUTS[2];
-  let pageList = [];
-  for (let i = 0; i < PAGE_VOLUME; i += 1) { pageList.push(i+1); }
+  const [PAGE_VOLUME, PAGE_SIZE, PAGE_SHOW] = lines[0].split(/\s/).map(Number);
+  // let pageList = [];
+  // for (let i = 0; i < PAGE_VOLUME; i += 1) { pageList.push(i+1); }
+  let pageList = [...Array(PAGE_VOLUME)].map((_, i) => i + 1);
   let startIndex = PAGE_SIZE * (PAGE_SHOW - 1),
       endIndex   = PAGE_SIZE * (PAGE_SHOW - 1) + PAGE_SIZE;
   let result = pageList.slice(startIndex, endIndex);
