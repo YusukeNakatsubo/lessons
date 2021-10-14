6ca5497112a157b47489f337e3c405c95d46be7e
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-14 11:54:58 +0900

    c053

--- c053.js
+++ c053.js
@@ -11,27 +11,20 @@ reader.on('line', (line) => {
 });
 reader.on('close', () => {
   const INPUTS  = lines,
-        COUNT   = Number(INPUTS[0]),
         NUMBERS = INPUTS[1].split(/\s/);
+  let multiplyNumber = 1,
+      newNumbers, maxNumber;
   if (NUMBERS.includes('x10') === true) {
-    let multiplyNumber = 'x10';
-    let newNumbers = NUMBERS.filter(item => item !== multiplyNumber).map(Number);
-    let result = 0;
-    if (newNumbers.includes(0) === true) { 
-      let maxNumber = Math.max(...newNumbers);
-      result = newNumbers.filter(item => item !== maxNumber);
-      console.log(result.reduce((a,b) => { return a + b }) * 10);
-    } else {
-      console.log(newNumbers.reduce((a,b) => { return a + b }) * 10);
-    }
+    multiplyNumber = 'x10';
+    newNumbers = NUMBERS.filter(item => item !== multiplyNumber).map(Number);
+    multiplyNumber = 10;
   } else {
-    let newNumbers = NUMBERS.map(Number);
-    if (newNumbers.includes(0) === true) { 
-      let maxNumber = Math.max(...newNumbers);
-      result = newNumbers.filter(item => item !== maxNumber);
-      console.log(result.reduce((a,b) => { return a + b }));
-    } else {
-      console.log(newNumbers.reduce((a,b) => { return a + b }));
-    }
+    newNumbers = NUMBERS.map(Number);
   }
+  if (newNumbers.includes(0) === true) {
+    maxNumber = Math.max(...newNumbers);
+    newNumbers = newNumbers.filter(item => item !== maxNumber);
+  }
+  let calculateNumbers = newNumbers.reduce((a,b) => { return a + b });
+  console.log(calculateNumbers * multiplyNumber);
 });
