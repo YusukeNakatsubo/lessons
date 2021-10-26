7e9972b23a4e0014e437d1f0139bf6f540f4cff9
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-26 13:36:42 +0900

    c065

--- c065.js
+++ c065.js
@@ -1,34 +1,32 @@
 // This is a code with a 100% pass rate.
-const INPUT = lines;
-const ENCRYPTION_COUNT = Number(INPUT[0]);
-const ENCRYPTION_LINE = INPUT[1];
-//
-const DECRYPYION_LINE = [];
-for (let i = 0; i < ENCRYPTION_LINE.length; i += 1) {
-  let tmp = 0;
-  if ((i + 1) % 2 === 1) { // odd
-    tmp = ENCRYPTION_COUNT * -1;
-  } else { // even
-    tmp = ENCRYPTION_COUNT;
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        COUNT  = Number(INPUTS[0]);
+  let result = [];
+  for (let i = 0; i < 100; i += 1) { result.push(i + 1); }
+  for (let i = 0; i < COUNT; i += 1) {
+    let tmp = INPUTS[i + 1].split(/\s/);
+    let tmpResult,
+        tmpString = String(tmp[0]),
+        tmpNumber = Number(tmp[1]);
+    if (tmpString === '>') {
+      tmpResult = result.filter((value) => { return value > tmpNumber });
+    } else if (tmpString === '<') {
+      tmpResult = result.filter((value) => { return value < tmpNumber });
+    } else if (tmpString === '/') {
+      tmpResult = result.filter((value) => { return value % tmpNumber === 0 });
+    }
+    result = tmpResult;
   }
-  // encode by unicode
-  let tmpFromCharCode = ENCRYPTION_LINE[i].codePointAt() + tmp;
-  const TMP_A = 'A'.codePointAt();
-  const TMP_Z = 'Z'.codePointAt();
-  // tmpFromCharCode < TMP_A
-  if (tmpFromCharCode < TMP_A) {
-    // Counting from Z
-    let tmp = String.fromCodePoint(TMP_Z - (TMP_A - tmpFromCharCode - 1));
-    DECRYPYION_LINE.push(tmp)
-  // tmpFromCharCode > TMP_Z
-  } else if (tmpFromCharCode > TMP_Z) {
-    // Counting from A
-    let tmp = String.fromCodePoint(TMP_A + (tmpFromCharCode - TMP_Z - 1));
-    DECRYPYION_LINE.push(tmp)
-  } else {
-  // others
-    DECRYPYION_LINE.push(String.fromCodePoint(tmpFromCharCode))
-  }
-}
-//
-console.log(DECRYPYION_LINE.join(''));
+  console.log(Number(result));
+});
