85e6b70fbe24041e80966d0c324989e99e6263de
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-11-04 13:29:57 +0900

    c073

--- c073.js
+++ c073.js
@@ -1,4 +1,3 @@
-// This is a code with a 60% pass rate. Ummm...
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 var lines = [];
@@ -14,14 +13,17 @@ reader.on('close', () => {
         DISATNCE = Number(INPUTS[0]),
         [USAGI_SPEED, HANDICAP_DISTANCE, HANDICAP_TIME] = INPUTS[1].split(/\s/).map(Number);
         KAME_SPEED = Number(INPUTS[2]);
-  const usagiHandicapTime = (Math.floor(DISATNCE / HANDICAP_DISTANCE) - 1) * HANDICAP_TIME;
+  let usagiHandicapTime = 0;
+  DISATNCE % HANDICAP_DISTANCE === 0
+  ? usagiHandicapTime = (Math.floor(DISATNCE / HANDICAP_DISTANCE) - 1) * HANDICAP_TIME
+  : usagiHandicapTime = (Math.floor(DISATNCE / HANDICAP_DISTANCE)) * HANDICAP_TIME;
   const usagiTime = DISATNCE * USAGI_SPEED + usagiHandicapTime;
   const kameTime  = DISATNCE * KAME_SPEED;
-  if (usagiTime === kameTime) {
-    console.log('DRAW');
+  if (usagiTime < kameTime) {
+    console.log('USAGI');
   } else if (usagiTime > kameTime) {
     console.log('KAME');
-  } else if (usagiTime < kameTime) {
-    console.log('USAGI')
+  } else {
+    console.log('DRAW');
   }
 });
