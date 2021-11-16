e9834d3bc95662dfc07fe7f8638cc2a4f3a04cd2
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-11-16 14:36:18 +0900

    c080

---
+++ c080.js
@@ -0,0 +1,25 @@
+// This is a code with a 100% pass rate.
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        [BTN_COUNT, JUDGE_COUNT] = INPUTS[0].split(/\s/).map(Number),
+        GAME_COUNT = Number(INPUTS[1]),
+        PUSH_BTNS = INPUTS[2].split(/\s/).map(Number);
+  let goodCount = 0,
+      badCount  = 0;
+  for (let i = 0; i < GAME_COUNT; i += 1) {
+    let tmp = i + 1;
+    if (tmp > BTN_COUNT) { tmp -= BTN_COUNT; }
+    PUSH_BTNS[i] === tmp ? goodCount += 1 : badCount += 1;
+  }
+  badCount >= JUDGE_COUNT ? console.log('-1') : console.log(goodCount * 1000);
+});
