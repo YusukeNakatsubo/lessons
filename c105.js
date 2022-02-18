b0d1642fb8a713db0dd52f80e0d36c2f8724533e
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2022-02-18 14:07:39 +0900

    c105

---
+++ c105.js
@@ -0,0 +1,33 @@
+// This is a code with a 90% pass rate.
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+  const INPUTS = lines,
+        COUNT  = Number(INPUTS[0]);
+
+  let cardArr = INPUTS[1].split(/\s/).map(Number);
+  let newCardArr = cardArr.sort((a, b) => { return a - b; });
+
+  let result  = 0;
+  for (let i = 0; i < COUNT - 1; i += 1) {
+    let previousCard = newCardArr[i],
+        currentCard  = newCardArr[i + 1];
+
+    let difference = currentCard - previousCard;
+    if (difference !== 1) {
+      result += previousCard;
+    }
+
+    if (i === COUNT - 2) result += currentCard;
+  }
+
+  console.log(result);
+});
