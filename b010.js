18e94061914436f07fef1a920e4628d1e5c0d7e4
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2022-02-18 16:47:42 +0900

    b010

--- b010.js
+++ b010.js
@@ -1,4 +1,4 @@
-// This is a code with a 70% pass rate.
+// This is a code with a 100% pass rate.
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 var lines = [];
@@ -15,42 +15,34 @@ reader.on('close', () => {
         TEAM_A_POSITION_ARR = INPUTS[1].split(/\s/).map(Number),
         TEAM_B_POSITION_ARR = INPUTS[2].split(/\s/).map(Number);
 
-  let passerPosition = 0,
-      passerNumber   = Number(PASSER_PLAYER_NUMBER) - 1;
-  if (PASSER_TEAM_NAME === 'A') passerPosition = TEAM_A_POSITION_ARR[passerNumber];
-  else if (PASSER_TEAM_NAME === 'B') passerPosition = TEAM_B_POSITION_ARR[passerNumber];
+  let passerNumber   = Number(PASSER_PLAYER_NUMBER) - 1,
+      passTeam       = TEAM_A_POSITION_ARR,
+      enemyTeam      = TEAM_B_POSITION_ARR,
+      maxNumber      = 110,
+      minNumber      = 55;
+  if (PASSER_TEAM_NAME === 'B') {
+    passTeam  = TEAM_B_POSITION_ARR,
+    enemyTeam = TEAM_A_POSITION_ARR,
+    maxNumber = 55,
+    minNumber = 0;
+  }
 
-  let offsideArr = [];
-  if (PASSER_TEAM_NAME === 'A') {
-    let newTeamBPositionArr = TEAM_B_POSITION_ARR.sort((a, b) => a - b);
-    for (let i = 0; i < 11; i += 1) {
-      if (TEAM_A_POSITION_ARR[i] > 55 && TEAM_A_POSITION_ARR[i] < 110 && TEAM_A_POSITION_ARR[i] > passerPosition && TEAM_A_POSITION_ARR[i] > newTeamBPositionArr[9]) {
-        offsideArr.push(TEAM_A_POSITION_ARR[i]);
+  let passerPosition     = passTeam[passerNumber],
+      newTeamPositionArr = enemyTeam.sort((a, b) => a - b),
+      counter            = 0;
+  for (let i = 0; i < 11; i += 1) {
+    if (PASSER_TEAM_NAME === 'A') {
+      if (passTeam[i] >= minNumber && passTeam[i] <= maxNumber && passTeam[i] > passerPosition && passTeam[i] > newTeamPositionArr[9]) {
+        console.log(i + 1);
+        counter += 1;
       }
-    }
-  } else if (PASSER_TEAM_NAME === 'B') {
-    let newTeamAPositionArr = TEAM_A_POSITION_ARR.sort((a, b) => a - b);
-    for (let i = 0; i < 11; i += 1) {
-      if (TEAM_B_POSITION_ARR[i] < 55 && TEAM_B_POSITION_ARR[i] > 0 && TEAM_B_POSITION_ARR[i] < passerPosition && TEAM_B_POSITION_ARR[i] < newTeamAPositionArr[1]) {
-        offsideArr.push(TEAM_B_POSITION_ARR[i]);
+    } else if (PASSER_TEAM_NAME === 'B') {
+      if (passTeam[i] <= maxNumber && passTeam[i] >= minNumber && passTeam[i] < passerPosition && passTeam[i] < newTeamPositionArr[1]) {
+        console.log(i + 1);
+        counter += 1;
       }
     }
   }
-
-  let resultArr = [];
-  for (let i = 0; i < offsideArr.length; i += 1) {
-    let index = 0;
-    if (TEAM_A_POSITION_ARR.includes(offsideArr[i]) === true) {
-      index = TEAM_A_POSITION_ARR.indexOf(offsideArr[i]) + 1;
-      resultArr.push(index);
-    }
-    if (TEAM_B_POSITION_ARR.includes(offsideArr[i]) === true) {
-      index = TEAM_B_POSITION_ARR.indexOf(offsideArr[i]) + 1;
-      resultArr.push(index);
-    }
-  }
-
-  if (resultArr.length !== 0) for (let i = 0; i < resultArr.length; i += 1) { console.log(resultArr[i]); }
-  else console.log('None');
+  if (counter === 0) console.log('None');
 
 });
