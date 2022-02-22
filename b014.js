6a92b383b6f509adff718fb8c07a0d99c3a988e6
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2022-02-22 15:48:23 +0900

    test

--- b014.js
+++ b014.js
@@ -1,47 +1,35 @@
-// b014
-// let X = 3;
-// let Y = 3;
-// let Z = 3;
-// X は立体の奥行きを、Y は立体の横幅を、Z は立体の高さを表す整数
+// 無理...解けない
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
 const INPUTS = lines,
-      [X, Y, Z] = INPUTS[0].split(/\s/).map(Number);
+[X, Y, Z] = INPUTS[0].split(/\s/).map(Number);
 
-const cubicArr = Array(Y).fill(Array(Z).fill('.'));
+let cubicArr = Array(Y).fill(Array(Z).fill(null))
+    zCounter = 0,
+    yCounter = 0;
 console.log(cubicArr);
-// [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
 
-// 各 (x,y,z) は '#' または '.' からなる一文字で、
-// この文字が '#' のときはセル (x,y,z) が立体に含まれることを、'.' のときはセル (x,y,z) が立体に含まれないことを意味します。
-
-// Z軸
-for (let k = 0; k < Z; k += 1) {
-  // X軸
-  for (let i = 0; i < X; i += 1) {
-    // '--' を除く
-    let tmp = INPUTS[i + 1];
-    // Y軸
-    for (let j = 0; j < Y; j += 1) {
-      if (tmp[j] === '#') cubicArr[k][j] = '#';
-      console.log(cubicArr);
+for (let i = 0; i < Z; i += 1) {
+  let tmpCellArr = INPUTS[i + 1].split('');
+  // if (tmpCellArr[0] === '-') {
+  //   continue;
+  // }
+  for (let j = 0; j < Y; j += 1) {
+    if (tmpCellArr[j] === '#') {
+      cubicArr[i][j] = '#';
+    } else if (cubicArr[i][j] === null || cubicArr[i][j] === '.') {
+      cubicArr[i][j] = '.';
     }
   }
 }
-// console.log(cubicArr);
-
-X,Y,Z=list(map(int,input().split()))
-m=[['.' for j in range(Y)] for i in range(Z)]
-print(m)
-
-for k in range(Z):
-    for i in range(X):
-        n=input()
-        for j in range(Y):
-            if n[j]=='#':
-                m[k][j]='#'
-    l=input()
-print(m)
-
-for i in reversed(range(Z)):
-    l=map(str,m[i])
-    s=''.join(l)
-    print(s)
+console.log(cubicArr);
+});
