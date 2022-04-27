c391e391f4fff83efd090ce4f707666b10dd024d
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2022-04-27 18:04:44 +0900

    a052

--- a052.js
+++ a052.js
@@ -1,3 +1,4 @@
+// This is a code with a 100% pass rate.
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 var lines = [];
@@ -11,66 +12,35 @@ reader.on('line', (line) => {
 reader.on('close', () => {
   const DATA   = lines,
         COUNT  = Number(DATA[0]),
-        [A, B] = DATA[1].split(/\s/).map(Number),
-        AB     = A + B;
+        [A, B] = DATA[1].split(/\s/).map(Number);
 
-  let resultArr = Array(COUNT).fill(1);
-  let tmpArr = [-1];
-  let aArr  = Array(COUNT).fill(0),
-      bArr  = Array(COUNT).fill(0),
-      abArr = Array(COUNT).fill(0);
+  let resultArr  = Array(COUNT).fill(0),
+      currentArr = [- 1];
 
-  // Aのみ考える
-  let indexA = 0;
-  while (COUNT >= indexA) {
-    if (indexA + (A - 1) < COUNT - 1) {
-      aArr[indexA + (A - 1)] = 1;
-    }
-    else {
-      let difference = COUNT - indexA;
-      if (difference <= A || difference <= B) aArr[COUNT - 1] = 1; 
-    }
-    indexA += A;
-    // console.log('Hey');
-  }
-  console.log(aArr);
-  
-  // Bのみ考える
-  let indexB = 0;
-  while (COUNT >= indexB) {
-    if (indexB + (B - 1) < COUNT - 1) {
-      bArr[indexB + (B - 1)] = 1;
-    }
-    else {
-      let difference = COUNT - indexB;
-      if (difference <= A || difference <= B) bArr[COUNT - 1] = 1; 
-    }
-    indexB += B;
-    // console.log('Hey');
-  }
-  console.log(bArr);
-     
-  // ABのみ考える
-  let indexAB = 0;
-  while (COUNT >= indexAB) {
-    if (indexAB + (AB - 1) < COUNT - 1) {
-      abArr[indexAB + (AB - 1)] = 1;
-    }
-    else {
-      let difference = COUNT - indexAB;
-      if (difference <= A || difference <= B) abArr[COUNT - 1] = 1; 
-    }
-    indexAB += AB;
-    // console.log('Hey');
-  }
-  console.log(abArr);
+    while (true) {
+      let nextArr = [];
 
-  for (let i = 0; i < COUNT; i += 1) {
-    if (aArr[i] === 0 && bArr[i] === 0 && abArr[i] === 0) resultArr[i] = 0;
-  }
-//   console.log(resultArr);
-  
+      for (let current of currentArr) {
+        if (current + A <= COUNT - 1) {
+           if (resultArr[current + A] === 0) {
+             nextArr.push(current + A);
+             resultArr[current + A] = 1;
+           }
+        }
+        if (current + B <= COUNT - 1) {
+           if (resultArr[current + B] === 0) {
+             nextArr.push(current + B);
+             resultArr[current + B] = 1;
+           }
+        }
+     }
+
+     if (nextArr.length === 0) break;
+     currentArr = nextArr;
+   }
+
+  if (resultArr.slice(-1)[0] === 0) resultArr.splice(-1, 1, 1);
+
   const result = resultArr.filter(el => el === 0).length;
   console.log(result);
-     
 });
