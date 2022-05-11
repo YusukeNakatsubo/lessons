a1d02d69d16ec944a25d9a234b3032621dc10cbf
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2022-05-11 14:40:27 +0900

    b029

--- b029.js
+++ b029.js
@@ -1,4 +1,4 @@
-// This is a code with a 50% pass rate.
+// This is a code with a 100% pass rate.
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 var lines = [];
@@ -29,33 +29,5 @@ reader.on('close', () => {
     result += sortDistacePointArr[i].price;
   }
 
-  console.log(Math.floor(result / K));
+  console.log(Math.round(result / K));
 });
-
-// This Python Code is a code with a 90% pass rate.
-
-// import math
-
-// x,y = map(int,input().rstrip().split(' '))
-// k = int(input())
-// N = int(input())
-// distancelist = [0 for i in range(N)]
-
-// # print(distancelist)
-
-// for i in range(N):
-//     xx,yy,p = map(int,input().rstrip().split(' '))
-//     score = math.sqrt((x - xx)**2 + (y - yy)**2)
-//     distancelist[i] = (i,score,p)
-
-// # print(distancelist)
-
-// sorted_distance = sorted(distancelist,key=lambda distance: distance[1])
-
-// # print(sorted_distance)
-
-// result = 0
-// for i in range(k):
-//     result += sorted_distance[i][2]
-
-// print(round(result/k))
